e8bf169a197dce2e92de541743a3dc55d272195b
LaboDJ <jacopolabardi@gmail.com>
2013-12-04 02:08:30 +0100

    Changes from Linux 3.4.0 -> 3.4.71
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/char/random.c
    	drivers/mmc/core/cd-gpio.c
    	drivers/staging/zsmalloc/zsmalloc-main.c
    	drivers/usb/host/xhci-hub.c
    	fs/ext4/super.c
    	kernel/futex.c
    	kernel/panic.c
    	kernel/time/timekeeping.c
    	mm/vmscan.c
    	net/wireless/reg.c
    
    Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>
    
    Conflicts:
    	block/partition-generic.c
    	drivers/base/regmap/regmap-debugfs.c
    	drivers/mmc/core/cd-gpio.c
    	drivers/staging/zsmalloc/zsmalloc-main.c
    	kernel/sched/core.c
    	kernel/time/timekeeping.c
    	net/netfilter/nf_conntrack_sip.c
    	net/wireless/reg.c
    
    Moto G Merge Linux Kernel Conflicts. 3.4.0 - 3.4.71
    
    Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

--- drivers/base/regmap/regmap-debugfs.c
+++ drivers/base/regmap/regmap-debugfs.c
@@ -206,6 +206,7 @@ static ssize_t regmap_map_read_file(struct file *file, char __user *user_buf,
 		if (p >= *ppos) {
 			/* ...but not beyond it */
 			if (buf_pos + map->debugfs_tot_len > count)
+			if (buf_pos + 1 + tot_len >= count)
 				break;
 
 			/* Format the register */
